18ddfafd59e9a8d4c34edd7f483e0f427077c025
zaingg <49590955+zaingg@users.noreply.github.com>
2019-04-18 23:54:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`splay | YouTube`,"NOOB GAMER SARA")
+client.user.setGame(`_play | YouTube`,"NOOB GAMER SARA")
 client.user.setStatus("dnd")
 });
 
